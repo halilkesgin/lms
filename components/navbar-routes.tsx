c23369275334112cedf18d8385555bcb2e4b6afe
halil kesgin <halillkesgin@gmail.com>
2023-10-22 17:09:04 +0300

    full project

---
+++ components/navbar-routes.tsx
@@ -0,0 +1,48 @@
+"use client"
+
+import {useAuth, UserButton} from "@clerk/nextjs"
+import { usePathname } from "next/navigation";
+import Link from "next/link";
+import { LogOut } from "lucide-react";
+import {isTeacher} from "@/lib/teacher";
+
+import { Button } from "@/components/ui/button";
+import {SearchInput} from "@/components/search-input";
+
+export const NavbarRoutes = () => {
+
+    const pathname = usePathname();
+    const { userId } = useAuth();
+    const isTeacherPage = pathname?.startsWith("/teacher");
+    const isCoursePage = pathname?.includes("/courses");
+    const isSearchPage = pathname === "/search";
+
+    return (
+        <>
+            {isSearchPage && (
+                <div className="hidden md:block">
+                    <SearchInput />
+                </div>
+            )}
+            <div className="flex gap-x-2 ml-auto">
+                {isTeacherPage || isCoursePage ? (
+                    <Link href="/">
+                        <Button size="sm" variant="ghost">
+                            <LogOut className="h-4 w-4 mr-2" />
+                            Exit
+                        </Button>
+                    </Link>
+                ) : isTeacher(userId) ?  (
+                    <Link href="/teacher/courses">
+                        <Button  size="sm" variant="ghost">
+                            Teacher mode
+                        </Button>
+                    </Link>
+                ) : null}
+                <UserButton
+                    afterSignOutUrl="/"
+                />
+            </div>
+        </>
+    )
+}
